3c46d122575ed25ec453cfe31c0006f319d1c6cb
Cs0kis <yusuf_Yasar96@hotmail.com>
2016-03-01 05:06:57 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/bscBot/source/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Cs0kis/source/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -240,9 +240,9 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Cs0kis/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
+        chatLink: "https://rawgit.com/Cs0kis/source/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -250,8 +250,8 @@
         settings: {
             botName: "basicBot",
             language: "english",
-            chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
-            scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
+            chatLink: "https://rawgit.com/Cs0kis/source/master/lang/en.json",
+            scriptLink: "https://rawgit.com/Cs0kis/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
@@ -309,9 +309,9 @@
             songstats: true,
             commandLiteral: "!",
             blacklists: {
-                NSFW: "https://rawgit.com/bscBot/custom/master/blacklists/NSFWlist.json",
-                OP: "https://rawgit.com/bscBot/custom/master/blacklists/OPlist.json",
-                BANNED: "https://rawgit.com/bscBot/custom/master/blacklists/BANNEDlist.json"
+                NSFW: "https://rawgit.com/Cs0kis/custom/master/blacklists/NSFWlist.json",
+                OP: "https://rawgit.com/Cs0kis/custom/master/blacklists/OPlist.json",
+                BANNED: "https://rawgit.com/Cs0kis/custom/master/blacklists/BANNEDlist.json"
             }
         },
         room: {
@@ -2141,11 +2141,11 @@
                             case 'es': ch += 'Por favor, hable Inglés.'; break;
                             case 'fr': ch += 'Parlez anglais, s\'il vous plaît.'; break;
                             case 'nl': ch += 'Spreek Engels, alstublieft.'; break;
-                            case 'pl': ch += 'Proszę mówić po angielsku.'; break;
+                            case 'pl': ch += 'Prosze mówic po angielsku.'; break;
                             case 'pt': ch += 'Por favor, fale Inglês.'; break;
                             case 'sk': ch += 'Hovorte po anglicky, prosím.'; break;
                             case 'cs': ch += 'Mluvte prosím anglicky.'; break;
-                            case 'sr': ch += 'Молим Вас, говорите енглески.'; break;
+                            case 'sr': ch += '????? ???, ???????? ????????.'; break;
                         }
                         ch += ' English please.';
                         API.sendChat(ch);
@@ -2476,7 +2476,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/bscBot/source/master/lang/langIndex.json", function (json) {
+                        $.get("https://rawgit.com/Cs0kis/source/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
