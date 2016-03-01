dfa274d9e385535ddef10a4bad873f226f488e2c
Cs0kis <yusuf_Yasar96@hotmail.com>
2016-03-01 05:01:23 +0200

    Update and rename plugdj.js to basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,22 +236,22 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "Sanene",
+        version: "2.8.15",
         status: false,
-        name: "Botlu",
+        name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Cs0kis/source/master/plugdj.js",
+        scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/bscBot/source/master/lang/tr.json",
+        chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
             botName: "basicBot",
-            language: "turkish",
-            chatLink: "https://rawgit.com/bscBot/source/master/lang/tr.json",
-            scriptLink: "https://rawgit.com/Cs0kis/source/master/plugdj.js",
+            language: "english",
+            chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
+            scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
